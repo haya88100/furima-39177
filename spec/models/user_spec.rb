31e565bd5a2cf6d949bcb3d12fedd11d1a82309e
haya88100 <134593297+haya88100@users.noreply.github.com>
2023-08-07 21:24:31 +0900

    テストコードの編集

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -22,11 +22,33 @@ RSpec.describe User, type: :model do
         @user.valid?
         expect(@user.errors.full_messages).to include("Email can't be blank")
       end
+      it "重複したメールアドレスは登録できない" do
+        @user.save
+        another_user = FactoryBot.build(:user, email: @user.email)
+        another_user.valid?
+        expect(another_user.errors.full_messages).to include("Email has already been taken")
+      end
+      it "メールアドレスに@を含まない場合は登録できない" do
+        @user.email = "invalid_email"
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Email is invalid")
+      end
+      it "パスワードが空では登録できない" do
+        @user.password = ""
+        @user.password_confirmation = ""
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password can't be blank")
+      end
       it "パスワードが5文字以下であれば登録できない" do
         @user.password = "12345"
         @user.password_confirmation = "12345"
         @user.valid?
         expect(@user.errors.full_messages).to include("Password is too short (minimum is 6 characters)")
+      end
+      it "パスワードとパスワード（確認用）が不一致だと登録できない" do
+        @user.password_confirmation = "mismatch_password"
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
       end
       it "パスワードが数字のみでは登録できない" do
         @user.password = "012345"
@@ -51,10 +73,20 @@ RSpec.describe User, type: :model do
         @user.valid?
         expect(@user.errors.full_messages).to include("Last name can't be blank")
       end
+      it "姓（全角）に半角文字が含まれていると登録できない" do
+        @user.last_name = "Yamada"
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Last name is invalid")
+      end
       it "名前（名）が空では登録できない" do
         @user.fast_name = ''
         @user.valid?
         expect(@user.errors.full_messages).to include("Fast name can't be blank")
+      end
+      it "名（全角）に半角文字が含まれていると登録できない" do
+        @user.fast_name = "Taro"
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Fast name is invalid")
       end
       it "名前（姓）のフリガナが空では登録できない" do
         @user.last_name_kana = ''
