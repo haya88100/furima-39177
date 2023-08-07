8236161a6600b74861a636c88daff31216e835db
haya88100 <134593297+haya88100@users.noreply.github.com>
2023-08-07 21:54:49 +0900

    バリテーションの編集

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,17 +7,17 @@ class User < ApplicationRecord
   validates :nickname, presence: true
   validates :birthday, presence: true
 
-  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, } do
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ } do
     validates :fast_name
     validates :last_name
   end
 
-  with_options presence: true, format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/,  } do
+  with_options presence: true, format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/  } do
     validates :fast_name_kana
     validates :last_name_kana
   end
 
-  validates :password, format: { with: /\A(?=.*[a-zA-Z])(?=.*\d)/,  }
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i  }
 
   
 end
