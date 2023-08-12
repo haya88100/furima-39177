ef0e27c16f4164263ee24567bc7c1a7a07d991c4
haya88100 <134593297+haya88100@users.noreply.github.com>
2023-08-12 09:20:04 +0900

    ActiveHashの設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   
   def index
-    @items = Item.all
+    #@items = Item.all
   end
 
   def new
@@ -9,13 +9,18 @@ class ItemsController < ApplicationController
   end
 
   def create
-
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   private
 
-  def message_params
-    params.require(:message).permit(:content, :image).merge(user_id: current_user.id)
+  def item_params
+    params.require(:item).permit(:content, :image).merge(user_id: current_user.id)
   end
 
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -2,14 +2,22 @@ class Item < ApplicationRecord
 
   validates :name, presence: true
   validates :memo, presence: true
-  validates :status_id, presence: true
-  validates :chage_bearer_id, presence: true
-  validates :shopping_area_id, presence: true
-  validates :delivary_day_id, presence: true
+  validates :status_id, presence: true, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :chage_bearer_id, presence: true, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :shopping_area_id, presence: true, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :delivary_day_id, presence: true, numericality: { other_than: 1 , message: "can't be blank"}
   validates :price, presence: true
   validates :user, presence: true
-  validates :category_id, presence: true
+  validates :category_id, presence: true, numericality: { other_than: 1 , message: "can't be blank"}
+
+ 
 
   belongs_to :user
   has_one_attached :image
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :status
+  belongs_to :shopping_area
+  belongs_to :chage_bearer
+  belongs_to :delivary_day
 end
