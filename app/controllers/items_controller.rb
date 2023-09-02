c0d02f1c61585009316f34097931663e31d71a9c
haya88100 <134593297+haya88100@users.noreply.github.com>
2023-09-02 15:36:43 +0900

    商品消去機能１

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,7 +2,7 @@ class ItemsController < ApplicationController
 
   before_action :authenticate_user!, only: [:new, :edit]
   before_action :move_to_index, except: [:index, :show]
-  before_action :set_item, only: [:show, :edit, :update]
+  before_action :set_item, only: [:show, :edit, :update, :destroy]
   before_action :redirect_if_not_owner, only: [:edit, :update]
   
   def index
@@ -36,6 +36,13 @@ class ItemsController < ApplicationController
     else
       render :edit
     end
+  end
+
+  def destroy
+    @item
+    @item.destroy
+
+    redirect_to root_path
   end
 
   private
