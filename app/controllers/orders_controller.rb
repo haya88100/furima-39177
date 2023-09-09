317f55c9e053939d8f8ef2c0b1492a7236204790
haya88100 <134593297+haya88100@users.noreply.github.com>
2023-09-09 14:44:14 +0900

    購入者情報の保存２

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,16 +2,17 @@ class OrdersController < ApplicationController
   before_action :authenticate_user!, except: :index
   
   def index
-  end
-  
-  def new
+    @item = Item.find(params[:item_id])
     @order_address = OrderAddress.new
   end
   
+  def new
+  end
+  
   def create
+    binding.pry
     @order_address = OrderAddress.new(order_params)
-    if @order_address.valid?
-      @order_address.save
+    if @order_address.save
       redirect_to root_path
     else
       render :new
@@ -21,7 +22,7 @@ class OrdersController < ApplicationController
   private
 
   def order_params
-    params.require(:order_address).permit(:postal_code, :shopping_area_id, :city, :house_number, :building_name, :phone_number).merge(user_id: current_user.id, item_id: item_id)
+    params.require(:order_address).permit(:postal_code, :shopping_area_id, :city, :house_number, :building_name, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
   end
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   get 'items/index'
   
   root to: "items#index"
-  resources :items, only: [:index, :new, :create, :show, :edit, :update, :destroy]
-  
-  resources :orders, only: [:index, :new, :create]
+  resources :items, only: [:index, :new, :create, :show, :edit, :update, :destroy] do
+    resources :orders, only: [:index, :new, :create]
+  end
 end
